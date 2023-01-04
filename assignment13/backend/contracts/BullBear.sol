15b3ca9972f7bcbb93642c085619d0a969488c3c
abdelrahman mostafa <abdelrahman.sheta16@gmail.com>
2023-01-04 10:40:03 +0200

    Update BullBear functionalities

--- assignment13/backend/contracts/BullBear.sol
+++ assignment13/backend/contracts/BullBear.sol
@@ -22,6 +22,30 @@ contract BullBear is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
+    AggregatorV3Interface public pricefeed;
+
+    /**
+    * Use an interval in seconds and a timestamp to slow execution of Upkeep
+    */
+    uint public /* immutable */ interval; 
+    uint public lastTimeStamp;
+
+    int256 public currentPrice;
+    
+    // IPFS URIs for the dynamic nft graphics/metadata.
+    // NOTE: These connect to my IPFS Companion node.
+    // You should upload the contents of the /ipfs folder to your own node for development.
+    string[] bullUrisIpfs = [
+        "https://ipfs.io/ipfs/QmRXyfi3oNZCubDxiVFre3kLZ8XeGt6pQsnAQRZ7akhSNs?filename=gamer_bull.json",
+        "https://ipfs.io/ipfs/QmRJVFeMrtYS2CUVUM2cHJpBV5aX2xurpnsfZxLTTQbiD3?filename=party_bull.json",
+        "https://ipfs.io/ipfs/QmdcURmN1kEEtKgnbkVJJ8hrmsSWHpZvLkRgsKKoiWvW9g?filename=simple_bull.json"
+    ];
+    string[] bearUrisIpfs = [
+        "https://ipfs.io/ipfs/Qmdx9Hx7FCDZGExyjLR6vYcnutUR8KhBZBnZfAPHiUommN?filename=beanie_bear.json",
+        "https://ipfs.io/ipfs/QmTVLyTSuiKGUEmb88BgXG3qNC8YgpHZiFbjHrXKH3QHEu?filename=coolio_bear.json",
+        "https://ipfs.io/ipfs/QmbKhBXVWmwrYsTPFYfroR2N7NAekAMxHUVg2CWks7i9qj?filename=simple_bear.json"
+    ];
+
 
     constructor() ERC721("BullBear", "BBTK") {}
 

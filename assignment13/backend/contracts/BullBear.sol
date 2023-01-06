141970e6721d3bc66f7a4825017e1c2bc7de4efe
abdelrahman mostafa <abdelrahman.sheta16@gmail.com>
2023-01-06 10:42:16 +0200

    Update BullBear functionalities

--- assignment13/backend/contracts/BullBear.sol
+++ assignment13/backend/contracts/BullBear.sol
@@ -48,7 +48,21 @@ contract BullBear is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
 
     event TokensUpdated(string marketTrend);
 
-    constructor() ERC721("BullBear", "BBTK") {}
+        // For testing with the mock on Rinkeby, pass in 10(seconds) for `updateInterval` and the address of my 
+    // deployed  MockPriceFeed.sol contract (0xD753A1c190091368EaC67bbF3Ee5bAEd265aC420).
+    constructor(uint updateInterval, address _pricefeed) ERC721("Bull&Bear", "BBTK") {
+        // Set the keeper update interval
+        interval = updateInterval; 
+        lastTimeStamp = block.timestamp;  //  seconds since unix epoch
+
+        // set the price feed address to
+        // BTC/USD Price Feed Contract Address on Rinkeby: https://rinkeby.etherscan.io/address/0xECe365B379E1dD183B20fc5f022230C044d51404
+        // or the MockPriceFeed Contract
+        pricefeed = AggregatorV3Interface(_pricefeed); // To pass in the mock
+
+        // set the price for the chosen currency pair.
+        currentPrice = getLatestPrice();
+    }
 
     function safeMint(address to, string memory uri) public onlyOwner {
         uint256 tokenId = _tokenIdCounter.current();

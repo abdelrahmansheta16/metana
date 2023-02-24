0977aea12d0c0e281fc777d37997a49ee7d81c33
abdelrahman mostafa <abdelrahman.sheta16@gmail.com>
2023-02-24 06:41:19 +0200

    Update Assignment11 functionalities

---
+++ assignment11/src/pages/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Home = () => {
+
+  return (
+      (
+        <div className="min-h-screen bg-gray-100">
+          {/* Navigation Bar */}
+          <div className="bg-blue-500 p-4 flex justify-between items-center">
+            <div className="flex space-x-2">
+            </div>
+          </div>
+        </div>
+      )
+  );
+};
+
+
+export default Home;

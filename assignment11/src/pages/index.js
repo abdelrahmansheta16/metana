9dce4c0f20f436846c715d269cb528046fb0a615
abdelrahman mostafa <abdelrahman.sheta16@gmail.com>
2023-02-24 06:52:53 +0200

    Update Assignment11 functionalities

--- assignment11/src/pages/index.js
+++ assignment11/src/pages/index.js
@@ -9,6 +9,15 @@ const Home = () => {
           {/* Navigation Bar */}
           <div className="bg-blue-500 p-4 flex justify-between items-center">
             <div className="flex space-x-2">
+              <select
+                value={selectedNetwork}
+                onChange={handleNetworkChange}
+                className="bg-white text-blue-500 border border-blue-500 rounded-md px-3 py-1"
+              >
+                <option value="mainnet">Ethereum</option>
+                <option value="sepolia">Sepolia</option>
+                {/* Add more network options here */}
+              </select>
             </div>
           </div>
         </div>

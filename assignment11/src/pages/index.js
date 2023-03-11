9e9d1a743dcf3c275294a3c3d4339b111f3b61c9
abdelrahman mostafa <abdelrahman.sheta16@gmail.com>
2023-03-11 07:00:08 +0200

    Update Assignment11 functionalities

--- assignment11/src/pages/index.js
+++ assignment11/src/pages/index.js
@@ -225,6 +225,9 @@ const Home = () => {
                       ))}
                     </select>
                   </div>
+                  <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-md">
+                    Send ERC-1155 Tokens
+                  </button>
                 </form>
               </div>
             </div>

31666e7f5248e0a22103f0fd4eec1ba6e4d19727
abdelrahman mostafa <abdelrahman.sheta16@gmail.com>
2023-04-01 07:32:33 +0200

    Update Assignment11 functionalities

--- assignment11/src/pages/login.js
+++ assignment11/src/pages/login.js
@@ -6,30 +6,8 @@ const { wordlist } = require("ethereum-cryptography/bip39/wordlists/english");
 import key from 'ethereum-cryptography/hdkey';
 import crypto from 'crypto';
 import { publicToAddress, secp256k1, toChecksumAddress } from 'ethereumjs-util';
-import { ethers } from 'ethers';
 
-// export async function getStaticProps() {
-//     try {
-//         // Read the contents of the text file
-//         const data = await fse.readFile('./src/utils/wordlist.txt', 'utf8')
-//         console.log(data)
-//         const wordList = data.split(/\s+|\n/).filter(word => word.trim() !== "");
-//         return {
-//             props: {
-//                 content: wordList,
-//             },
-//         };
-//     } catch (error) {
-//         console.log(error)
-//         console.error('Error reading the file:', error);
-//         return {
-//             props: {
-//                 content: 'Error reading the file',
-//             },
-//         };
-//     }
-// }
-const RecoveryPage = ({ content }) => {
+const RecoveryPage = () => {
     const [mnemonic, setMnemonic] = useState('');
     const [error, setError] = useState('');
     const router = useRouter();

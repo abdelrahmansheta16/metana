6b18e66b04a5550c5d27db3ff407ae766f2956a3
abdelrahman mostafa <abdelrahman.sheta16@gmail.com>
2023-10-07 08:32:04 +0300

    Add Forging Contract Testing

--- assignment4/test/forgingContract.js
+++ assignment4/test/forgingContract.js
@@ -35,179 +35,156 @@ describe("ForgingContract", function () {
     });
   });
 
-  describe("sellBack", function () {
+  describe("burnToMintToken3", function () {
     describe("Validations", function () {
-      it("Should revert with the right error if amount not greater than 0", async function () {
-        const { tokenSale } = await loadFixture(deployOneYearLockFixture);
-        const amount = ethers.utils.parseEther("100");
+      it("Should revert with the right error if amount of token 0 is greater than balance of sender", async function () {
+        const { forging } = await loadFixture(deployOneYearLockFixture);
+        await expect(forging.burnToMintToken3(1000)).to.be.revertedWith(
+          "Insufficient balance of token 0"
+        );
+      });
 
-        await expect(tokenSale.sellBack(amount)).to.be.revertedWith(
+      it("Should revert with the right error if amount of token 1 is greater than balance of sender", async function () {
+        const { forging } = await loadFixture(deployOneYearLockFixture);
+        await expect(forging.burnToMintToken3(1000)).to.be.revertedWith(
+          "Insufficient balance of token 0"
+        );
+      });
+    });
+
+    describe("Burn", function () {
+      it("Should burn the required amount of token 0 from the sender", async function () {
+        const { forging, owner } = await loadFixture(
+          deployOneYearLockFixture
+        );
+        const before0 = forging.balanceOf(owner.address, 0);
+        await forging.burnToMintToken3(10);
+        const after0 = forging.balanceOf(owner.address, 0);
+        expect(before0).to.equal(after0 + 10);
+      });
+      it("Should burn the required amount of token 1 from the sender", async function () {
+        const { forging, owner } = await loadFixture(
+          deployOneYearLockFixture
+        );
+        const before1 = forging.balanceOf(owner.address, 0);
+        await forging.burnToMintToken3(10);
+        const after1 = forging.balanceOf(owner.address, 0);
+        expect(before1).to.equal(after1 + 10);
+      });
+    });
+    describe("Mint", function () {
+      it("Should mint the required amount of token 3 to the sender", async function () {
+        const { forging, owner } = await loadFixture(
+          deployOneYearLockFixture
+        );
+        const before3 = forging.balanceOf(owner.address, 0);
+        await forging.burnToMintToken3(10);
+        const after3 = forging.balanceOf(owner.address, 0);
+        expect(before3).to.equal(after3 - 10);
+      });
+    });
+  });
+  describe("tradeTokens", function () {
+    describe("Validations", function () {
+      it("Should revert with the right error if from tokenID is out of range", async function () {
+        const { forging } = await loadFixture(deployOneYearLockFixture);
+        await expect(forging.tradeTokens(3, 1, 2)).to.be.revertedWith(
+          "Invalid 'from' token ID"
+        );
+      });
+
+      it("Should revert with the right error if to tokenID is out of range", async function () {
+        const { forging } = await loadFixture(deployOneYearLockFixture);
+        await expect(forging.tradeTokens(0, 3, 2)).to.be.revertedWith(
+          "Invalid 'to' token ID"
+        );
+      });
+      it("Should revert with the right error if amount is not greater than 0", async function () {
+        const { forging } = await loadFixture(deployOneYearLockFixture);
+        await expect(forging.tradeTokens(0, 1, 0)).to.be.revertedWith(
+          "Amount must be greater than 0"
+        );
+      });
+    });
+
+    describe("Burn", function () {
+      it("Should burn the required amount of the 'from' token from the sender", async function () {
+        const { forging, owner } = await loadFixture(
+          deployOneYearLockFixture
+        );
+        const before0 = forging.balanceOf(owner.address, 0);
+        await forging.tradeTokens(0, 1, 10);
+        const after0 = forging.balanceOf(owner.address, 0);
+        expect(before0).to.equal(after0 + 10);
+      });
+    });
+    describe("Mint", function () {
+      it("Should mint the required amount of the 'to' token to the sender", async function () {
+        const { forging, owner } = await loadFixture(
+          deployOneYearLockFixture
+        );
+        const before1 = forging.balanceOf(owner.address, 0);
+        await forging.tradeTokens(0, 1, 10);
+        const after1 = forging.balanceOf(owner.address, 0);
+        expect(before1).to.equal(after1 - 10);
+      });
+    });
+  });
+  describe("mintTokens", function () {
+    describe("Validations", function () {
+      it("Should revert with the right error if tokenID is out of range", async function () {
+        const { forging } = await loadFixture(deployOneYearLockFixture);
+        await expect(forging.mintTokens(3, 2)).to.be.revertedWith(
+          "Invalid token ID"
+        );
+      });
+
+      it("Should revert with the right error if amount is not greater than 0", async function () {
+        const { forging } = await loadFixture(deployOneYearLockFixture);
+        await expect(forging.mintTokens(1, 0)).to.be.revertedWith(
           "Amount must be greater than 0"
         );
       });
 
-      it("Should revert with the right error if insufficient balance", async function () {
-        const amount = ethers.utils.parseEther("100");
-
-        const { tokenSale, otherAccount } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        // We use lock.connect() to send a transaction from another account
-        await expect(tokenSale.connect(otherAccount).sellBack(amount)).to.be.revertedWith(
-          "Insufficient balance"
+      it("Should revert with the right error if minting cool down is not over yet", async function () {
+        const { forging } = await loadFixture(deployOneYearLockFixture);
+        await forging.mintTokens(1, 3)
+        await expect(forging.mintTokens(1, 3)).to.be.revertedWith(
+          "Minting cooldown not over"
         );
       });
-
-      it("Should not revert if sufficient balance", async function () {
-        const amount = ethers.utils.parseEther("100");
-
-        const { tokenSale, otherAccount } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        await tokenSale.connect(otherAccount).purchaseTokens();
-        // We use lock.connect() to send a transaction from another account
-        await expect(tokenSale.connect(otherAccount).sellBack(amount)).not.to.be.reverted;
-      });
-
-      it("Should revert if contract balance is less than amount needed", async function () {
-        const amount = ethers.utils.parseEther("100");
-        const { tokenSale, otherAccount } = await loadFixture(
-          deployOneYearLockFixture
-        );
-        // Get the contract's address
-        const contractAddress = tokenSale.address;
-        await ethers.provider.sendTransaction({
-          to: contractAddress,
-          value: ethers.utils.parseEther("10"), // Increase the balance by 1000 ETH
-        });
-        // Get the balance of the contract
-        const contractBalance = await ethers.provider.getBalance(contractAddress);
-        console.log("contract balance: ", contractBalance);
-        await tokenSale.connect(otherAccount).purchaseTokens();
-        await expect(tokenSale.connect(otherAccount).sellBack(amount)).to.be.revertedWith(
-          "Insufficient Ether in the contract"
-        );
+      it("Should not revert if minting cool down is over", async function () {
+        const { forging } = await loadFixture(deployOneYearLockFixture);
+        await forging.mintTokens(1, 3)
+        await time.increase(600);
+        await expect(forging.mintTokens(1, 3)).not.to.be.reverted;
       });
     });
 
     describe("Events", function () {
-      it("Should emit an event on transfer", async function () {
-        const amount = ethers.utils.parseEther("100");
-        const refundAmount = ((amount * 10 ** 18) / 2000);
-        const { tokenSale, otherAccount } = await loadFixture(
-          deployOneYearLockFixture
-        );
-        // Get the contract's address
-        const contractAddress = tokenSale.address;
-        await ethers.provider.sendTransaction({
-          to: contractAddress,
-          value: ethers.utils.parseEther("1000"), // Increase the balance by 1000 ETH
-        });
-        // Get the balance of the contract
-        const contractBalance = await ethers.provider.getBalance(contractAddress);
-        console.log("contract balance: ", contractBalance);
-        await tokenSale.connect(otherAccount).purchaseTokens();
-        await tokenSale.connect(otherAccount).sellBack(amount).to.emit(tokenSale, "TokensSoldBack").withArgs(otherAccount, amount, refundAmount);
-      });
-    });
-
-    describe("Transfers", function () {
-      it("Should transfer the funds to the owner", async function () {
-        const amount = ethers.utils.parseEther("100");
-        const refundAmount = ((amount * 10 ** 18) / 2000);
-        const { tokenSale, otherAccount } = await loadFixture(
-          deployOneYearLockFixture
-        );
-        // Get the contract's address
-        const contractAddress = tokenSale.address;
-        await ethers.provider.sendTransaction({
-          to: contractAddress,
-          value: ethers.utils.parseEther("1000"), // Increase the balance by 1000 ETH
-        });
-        // Get the balance of the contract
-        const contractBalance = await ethers.provider.getBalance(contractAddress);
-        console.log("contract balance: ", contractBalance);
-        await tokenSale.connect(otherAccount).purchaseTokens();
-        await tokenSale.connect(otherAccount).sellBack(amount).to.changeEtherBalances(
-          [otherAccount, tokenSale],
-          [refundAmount, -refundAmount]
-        );
-      });
-    });
-  });
-  describe("purchaseTokens", function () {
-    describe("Validations", function () {
-      it("Should revert with the right error if supply exceed limit", async function () {
-        const { tokenSale, owner } = await loadFixture(deployOneYearLockFixture);
-        const amount = ethers.utils.parseEther("10000000");
-        await tokenSale._mint(owner, amount)
-        await expect(tokenSale.purchaseTokens()).to.be.revertedWith(
-          "Token sale has ended"
-        );
-      });
-
-      it("Should revert with the right error if value send is not equal to 1 ether", async function () {
-        const { tokenSale, owner } = await loadFixture(deployOneYearLockFixture);
-        const amount = ethers.utils.parseEther("1000");
-        await tokenSale._mint(owner, amount)
-        await expect(tokenSale.purchaseTokens({ value: ethers.utils.parseEther("2") })).to.be.revertedWith(
-          "You must send 1 Ether to purchase tokens"
-        );
-      });
-
-      it("Should revert with the right error if total supply and tokens to mint is greater than supply limit", async function () {
-        const { tokenSale, owner } = await loadFixture(deployOneYearLockFixture);
-
-        const amount = ethers.utils.parseEther("999001");
-        await tokenSale._mint(owner, amount)
-        await expect(tokenSale.purchaseTokens({ value: ethers.utils.parseEther("1") })).to.be.revertedWith(
-          "Exceeds total supply limit"
-        );
-      });
-    });
-
-    describe("Events", function () {
-      it("Should emit an event on withdrawals", async function () {
-        const { tokenSale, owner } = await loadFixture(deployOneYearLockFixture);
-        const amount = ethers.utils.parseEther("1000");
-        const refundAmount = ((amount * 10 ** 18) / 2000);
-        await tokenSale._mint(owner, amount)
-        await expect(tokenSale.purchaseTokens({ value: ethers.utils.parseEther("1") })).to.emit(tokenSale, "TokensPurchased").withArgs(owner, amount, refundAmount);
+      it("Should emit an event on minting", async function () {
+        const { forging, owner } = await loadFixture(deployOneYearLockFixture);
+        await expect(forging.mintTokens(1, 3)).to.emit(forging, "Minted").withArgs(owner, 1, 3);
       });
     });
 
     describe("Mint", function () {
       it("Should mint the tokens to the sender", async function () {
-        const { tokenSale, owner } = await loadFixture(deployOneYearLockFixture);
-        const amount = ethers.utils.parseEther("1000");
-        await tokenSale._mint(owner, amount)
-        await expect(tokenSale.purchaseTokens({ value: ethers.utils.parseEther("1") })).to.changeEtherBalances(
-          [owner, tokenSale],
-          [amount, -amount]
-        );
-      });
-    });
-  });
-  describe("withdrawEther", function () {
-    describe("Validations", function () {
-      it("Should revert with the right error if called from another account", async function () {
-        const { tokenSale, otherAccount } = await loadFixture(deployOneYearLockFixture);
-        await expect(tokenSale.connect(otherAccount).withdrawEther()).to.be.revertedWith(
-          "Ownable: caller is not the owner"
-        );
+        const { forging, owner } = await loadFixture(deployOneYearLockFixture);
+        const before0 = forging.balanceOf(owner.address, 0);
+        await forging.mintTokens(0, 10);
+        const after0 = forging.balanceOf(owner.address, 0);
+        expect(before0).to.equal(after0 - 10);
       });
     });
 
-    describe("Transfers", function () {
-      it("Should transfer the funds to the owner", async function () {
-        const { tokenSale, owner } = await loadFixture(deployOneYearLockFixture);
-        const contractBalance = await ethers.provider.getBalance(tokenSale.address);
-        await expect(tokenSale.withdrawEther()).to.changeEtherBalances(
-          [owner, tokenSale],
-          [contractBalance, -contractBalance]
-        );
+    describe("lastMintTime", function () {
+      it("Should update the lastMintTime state variable after minting the tokens", async function () {
+        const { forging, owner } = await loadFixture(deployOneYearLockFixture);
+        await forging.mintTokens(1, 3)
+        const lastMintTime = await forging.lastMintTime(owner)
+        const currentTime = await time.latest();
+        expect(lastMintTime).to.be.equal(currentTime);
       });
     });
   });

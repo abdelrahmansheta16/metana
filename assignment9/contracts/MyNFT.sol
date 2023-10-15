2ed07915046199b4a67cf46213e7d21787447817
abdelrahman mostafa <abdelrahman.sheta16@gmail.com>
2023-10-15 15:42:17 +0300

    Update Assignment9

---
+++ assignment9/contracts/MyNFT.sol
@@ -0,0 +1,85 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+//0xAf30E229e5f538066038443B726FBa2a25b5c3CE
+contract MyNFT is
+    ERC721,
+    ERC721Enumerable,
+    ERC721URIStorage,
+    ERC721Burnable,
+    Ownable
+{
+    string[] tokenURIs = [
+        "https://ipfs.io/ipfs/Qmdx9Hx7FCDZGExyjLR6vYcnutUR8KhBZBnZfAPHiUommN?filename=beanie_bear.json",
+        "https://ipfs.io/ipfs/QmTVLyTSuiKGUEmb88BgXG3qNC8YgpHZiFbjHrXKH3QHEu?filename=coolio_bear.json",
+        "https://ipfs.io/ipfs/QmbKhBXVWmwrYsTPFYfroR2N7NAekAMxHUVg2CWks7i9qj?filename=simple_bear.json",
+        "https://ipfs.io/ipfs/QmRXyfi3oNZCubDxiVFre3kLZ8XeGt6pQsnAQRZ7akhSNs?filename=gamer_bull.json",
+        "https://ipfs.io/ipfs/QmRJVFeMrtYS2CUVUM2cHJpBV5aX2xurpnsfZxLTTQbiD3?filename=party_bull.json",
+        "https://ipfs.io/ipfs/QmdcURmN1kEEtKgnbkVJJ8hrmsSWHpZvLkRgsKKoiWvW9g?filename=simple_bull.json",
+        "https://ipfs.io/ipfs/QmbKhBXVWmwrYsTPFYfroR2N7NAekAMxHUVg2CWks7i9qj?filename=simple_bear.json",
+        "https://ipfs.io/ipfs/QmRXyfi3oNZCubDxiVFre3kLZ8XeGt6pQsnAQRZ7akhSNs?filename=gamer_bull.json",
+        "https://ipfs.io/ipfs/QmRJVFeMrtYS2CUVUM2cHJpBV5aX2xurpnsfZxLTTQbiD3?filename=party_bull.json",
+        "https://ipfs.io/ipfs/QmdcURmN1kEEtKgnbkVJJ8hrmsSWHpZvLkRgsKKoiWvW9g?filename=simple_bull.json"
+    ];
+
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("MyNFT", "MNT") {
+        createInitialTokens(tokenURIs);
+    }
+
+    function createInitialTokens(string[] memory _tokenURIs) private onlyOwner {
+        for (uint256 i = 0; i < _tokenURIs.length; i++) {
+            safeMint(msg.sender, _tokenURIs[i]);
+        }
+    }
+
+    function safeMint(address to, string memory uri) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId,
+        uint256 batchSize
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId, batchSize);
+    }
+
+    function _burn(
+        uint256 tokenId
+    ) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    )
+        public
+        view
+        override(ERC721, ERC721Enumerable, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
---
+++ assignment9/contracts/StakeContract.sol
@@ -0,0 +1,124 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+// import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+//0xd0C504B3A6BD5191BB79c6D6c8c659afB9187acD
+interface MyNFT {
+    function ownerOf(uint256 tokenId) external view returns (address);
+
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) external;
+
+    function setApprovalForAll(address operator, bool approved) external;
+}
+
+interface MyToken {
+    function balanceOf(address account) external returns (uint256);
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) external returns (bool);
+
+    function transfer(address to, uint256 amount) external returns (bool);
+
+    function approveContract(
+        address owner,
+        address spender,
+        uint256 amount
+    ) external;
+
+    function mint(address to, uint256 amount) external;
+}
+
+contract StakingContract {
+    MyNFT immutable nftContract;
+    MyToken immutable tokenContract;
+    address private nftsHolder;
+    uint256 public stakingDuration = 1 days;
+    uint256 public rewardAmount = 10 * 10 ** 18; // 10 tokens with 18 decimals
+
+    mapping(address => uint256) public stakingTimestamp;
+    mapping(uint256 => bool) public isDeposited;
+    mapping(uint256 => address) public originalOwner;
+
+    constructor(address _stakingNFTAddress, address _erc20TokenAddress) {
+        nftContract = MyNFT(_stakingNFTAddress);
+        tokenContract = MyToken(_erc20TokenAddress);
+        nftsHolder = msg.sender;
+    }
+
+    // function onERC721Received(
+    //     address ,
+    //     address from,
+    //     uint256 tokenId,
+    //     bytes calldata
+    // ) external returns (bytes4){
+    //     originalOwner[tokenId] = from;
+    //     return IERC721Receiver.onERC721Received.selector;
+    // }
+
+    function getNFT(uint256 tokenId) external {
+        require(
+            nftContract.ownerOf(tokenId) != msg.sender,
+            "You already own this NFT"
+        );
+        require(
+            stakingTimestamp[msg.sender] + stakingDuration <= block.timestamp,
+            "Staking not available yet"
+        );
+        require(
+            tokenContract.balanceOf(msg.sender) >= rewardAmount,
+            "Not enough balance"
+        );
+        tokenContract.approveContract(msg.sender, address(this), rewardAmount);
+        tokenContract.transferFrom(msg.sender, address(this), rewardAmount);
+        nftContract.safeTransferFrom(nftsHolder, msg.sender, tokenId);
+        stakingTimestamp[msg.sender] = block.timestamp;
+    }
+
+    function depositNFT(uint256 tokenId) external {
+        require(
+            nftContract.ownerOf(tokenId) == msg.sender,
+            "You do not own this NFT"
+        );
+        require(!isDeposited[tokenId], "You already deposited this NFT");
+        isDeposited[tokenId] = true;
+        stakingTimestamp[msg.sender] = block.timestamp;
+    }
+
+    function withdrawTokens(uint256 tokenId) external {
+        require(
+            nftContract.ownerOf(tokenId) == msg.sender,
+            "You do not own this NFT"
+        );
+        uint256 periods = (block.timestamp - stakingTimestamp[msg.sender]) /
+            (5 seconds);
+        require(periods > 0, "Withdrawal not available yet");
+        require(isDeposited[tokenId], "You must deposit your NFT first");
+        tokenContract.mint(msg.sender, rewardAmount * periods);
+        stakingTimestamp[msg.sender] = block.timestamp;
+    }
+
+    function withdrawNFT(uint256 tokenId) external {
+        require(
+            nftContract.ownerOf(tokenId) == msg.sender,
+            "You do not own this NFT"
+        );
+        isDeposited[tokenId] = false;
+        nftContract.safeTransferFrom(address(this), msg.sender, tokenId);
+    }
+
+    function changeRewardAmount(uint256 _newRewardAmount) external {
+        rewardAmount = _newRewardAmount;
+    }
+
+    function changeStakingDuration(uint256 _newStakingDuration) external {
+        stakingDuration = _newStakingDuration;
+    }
+}

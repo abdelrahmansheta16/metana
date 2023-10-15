157fe528cf7a30c4f1655eacf6dface3ec4887b1
abdelrahman mostafa <abdelrahman.sheta16@gmail.com>
2023-10-15 16:50:36 +0300

    Update Assignment9

---
+++ assignment9/contracts/MyNFT v2.sol
@@ -0,0 +1,113 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
+
+contract MyNFTUpgradeableV2 is
+    Initializable,
+    ERC721Upgradeable,
+    ERC721EnumerableUpgradeable,
+    ERC721URIStorageUpgradeable,
+    ERC721BurnableUpgradeable,
+    OwnableUpgradeable
+{
+    string[] tokenURIs = [
+        "https://ipfs.io/ipfs/Qmdx9Hx7FCDZGExyjLR6vYcnutUR8KhBZBnZfAPHiUommN?filename=beanie_bear.json",
+        "https://ipfs.io/ipfs/QmTVLyTSuiKGUEmb88BgXG3qNC8YgpHZiFbjHrXKH3QHEu?filename=coolio_bear.json",
+        "https://ipfs.io/ipfs/QmbKhBXVWmwrYsTPFYfroR2N7NAekAMxHUVg2CWks7i9qj?filename=simple_bear.json",
+        "https://ipfs.io/ipfs/QmRXyfi3oNZCubDxiVFre3kLZ8XeGt6pQsnAQRZ7akhSNs?filename=gamer_bull.json",
+        "https://ipfs.io/ipfs/QmRJVFeMrtYS2CUVUM2cHJpBV5aX2xurpnsfZxLTTQbiD3?filename=party_bull.json",
+        "https://ipfs.io/ipfs/QmdcURmN1kEEtKgnbkVJJ8hrmsSWHpZvLkRgsKKoiWvW9g?filename=simple_bull.json",
+        "https://ipfs.io/ipfs/QmbKhBXVWmwrYsTPFYfroR2N7NAekAMxHUVg2CWks7i9qj?filename=simple_bear.json",
+        "https://ipfs.io/ipfs/QmRXyfi3oNZCubDxiVFre3kLZ8XeGt6pQsnAQRZ7akhSNs?filename=gamer_bull.json",
+        "https://ipfs.io/ipfs/QmRJVFeMrtYS2CUVUM2cHJpBV5aX2xurpnsfZxLTTQbiD3?filename=party_bull.json",
+        "https://ipfs.io/ipfs/QmdcURmN1kEEtKgnbkVJJ8hrmsSWHpZvLkRgsKKoiWvW9g?filename=simple_bull.json"
+    ];
+
+    bool public godModeEnabled; // Flag to enable God mode
+
+    using CountersUpgradeable for CountersUpgradeable.Counter;
+    CountersUpgradeable.Counter private _tokenIdCounter;
+
+    function initialize() external initializer {
+        __ERC721_init("MyNFT", "MNT");
+        __Ownable_init();
+        godModeEnabled = false;
+        createInitialTokens(tokenURIs);
+    }
+
+    function createInitialTokens(string[] memory _tokenURIs) private onlyOwner {
+        for (uint256 i = 0; i < _tokenURIs.length; i++) {
+            safeMint(msg.sender, _tokenURIs[i]);
+        }
+    }
+
+    function toggleGodMode() public onlyOwner {
+        godModeEnabled = !godModeEnabled; // Toggle God mode
+    }
+
+    function safeMint(address to, string memory uri) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    function godModeTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public onlyOwner {
+        require(godModeEnabled, "God mode is not enabled.");
+        _transfer(from, to, tokenId);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId,
+        uint256 batchSize
+    ) internal override(ERC721Upgradeable, ERC721EnumerableUpgradeable) {
+        super._beforeTokenTransfer(from, to, tokenId, batchSize);
+    }
+
+    function _burn(
+        uint256 tokenId
+    ) internal override(ERC721Upgradeable, ERC721URIStorageUpgradeable) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    )
+        public
+        view
+        override(ERC721Upgradeable, ERC721URIStorageUpgradeable)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    )
+        public
+        view
+        override(
+            ERC721Upgradeable,
+            ERC721EnumerableUpgradeable,
+            ERC721URIStorageUpgradeable
+        )
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
--- assignment9/scripts/deployNFTUpgradeable upgrade.js
+++ assignment9/scripts/deployNFTUpgradeable upgrade.js
@@ -1,14 +1,13 @@
 // scripts/deploy_upgradeable_box.js
 const { ethers, upgrades } = require("hardhat");
 
+const PROXY = "";
+
 async function main() {
-  const Contract = await ethers.getContractFactory("MyNFTUpgradeable");
-  console.log("Deploying MyNFTUpgradeable...");
-  const contract = await upgrades.deployProxy(Contract, [], {
-    initializer: "initialize",
-  });
-  await contract.deployed();
-  console.log("MyNFTUpgradeable deployed to:", box.address);
+  const Contract = await ethers.getContractFactory("MyNFTUpgradeableV2");
+  console.log("Upgrading MyNFTUpgradeable...");
+  await upgrades.upgradeProxy(PROXY, Contract);
+  console.log("MyNFTUpgradeable upgraded");
 }
 
 main();

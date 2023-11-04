507386697c169666b004ce104ffd32b8b977a66a
abdelrahman mostafa <abdelrahman.sheta16@gmail.com>
2023-11-04 12:56:52 +0200

    Update BullBear functionalities

--- assignment13/backend/scripts/BullBear_deploy.js
+++ assignment13/backend/scripts/BullBear_deploy.js
@@ -2,7 +2,7 @@ const hre = require("hardhat");
 
 async function main() {
 	//BTC/USD = 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43
-	//Contract deployed at 0xB2a92b6d6BbB87d136D5Eb62731B9b3Df7852E8A
+	//Contract deployed at 	
 	const Contract = await hre.ethers.getContractFactory("BullBear");
 	const duration = 60 * 60 * 24;
 	const contract = await Contract.deploy(duration, "0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43");

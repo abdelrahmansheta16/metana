4d12ad3838c08b7a94d55ecb62baf6a339c4a823
abdelrahman mostafa <abdelrahman.sheta16@gmail.com>
2023-11-22 17:58:53 +0200

    Update assignment8 functionalities

--- assignment8/contracts/MerkleTree.sol
+++ assignment8/contracts/MerkleTree.sol
@@ -3,12 +3,14 @@ pragma solidity 0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import {BitMaps} from "@openzeppelin/contracts/utils/structs/BitMaps.sol";
 import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
-contract AirDropToken is ERC721, ERC721Burnable, Ownable {
+import "hardhat/console.sol";
 
+contract AirDropToken is ERC721, ERC721Burnable, ERC721Enumerable, Ownable {
     error CustomError(string message);
 
     enum Stages {
@@ -29,7 +31,7 @@ contract AirDropToken is ERC721, ERC721Burnable, Ownable {
     address[] public mintedTokens;
     mapping(address => uint256) public revealBlockNumber;
     uint256 public numOfWhiteListMembers;
-    uint public totalSupply;
+    uint public totalSupplyLimit;
     bool public revealed;
     mapping(address => bytes32) public commitment;
     mapping(address => uint256) public nftAllocations;
@@ -38,49 +40,56 @@ contract AirDropToken is ERC721, ERC721Burnable, Ownable {
     event AllocationRevealed(address recipient, uint256 nftID);
 
     modifier onlyBeforeReveal() {
-        if(
-            block.number > revealBlockNumber[msg.sender]
-        ){
+        if (
+            block.number > revealBlockNumber[msg.sender] ||
+            revealBlockNumber[msg.sender] == 0
+        ) {
             revert CustomError("Airdrop has been already revealed");
         }
         _;
     }
 
     modifier onlyAfterReveal() {
-        if(
-            block.number < revealBlockNumber[msg.sender]
-        ){
-            revert CustomError("You can't claim tokens before the airdrop was revealed");
+        if (
+            block.number < revealBlockNumber[msg.sender] ||
+            revealBlockNumber[msg.sender] == 0
+        ) {
+            revert CustomError(
+                "You can't claim tokens before the airdrop was revealed"
+            );
         }
         _;
     }
 
     modifier atStage(Stages _stage) {
-        require(stage == _stage);
+        if (stage != _stage) {
+            revert CustomError("Wrong Stage!");
+        }
         _;
     }
 
     modifier timedTransitions() {
+        _;
         if (
             stage == Stages.PreMinting &&
             mintedTokens.length >= numOfWhiteListMembers
         ) {
             nextStage();
         } else if (
-            stage == Stages.PostMinting && mintedTokens.length >= totalSupply
+            stage == Stages.PostMinting &&
+            mintedTokens.length >= totalSupplyLimit
         ) {
             nextStage();
         }
-        _;
     }
 
     constructor(
         bytes32 _merkleRoot,
-        uint _totalSupply,
+        uint _totalSupplyLimit,
         uint _numOfWhiteListMembers
-    ) ERC721("AirDropToken", "ADT") Ownable(msg.sender) {
+    ) payable ERC721("AirDropToken", "ADT") Ownable(msg.sender) {
         revealed = false;
-        totalSupply = _totalSupply;
+        totalSupplyLimit = _totalSupplyLimit;
         merkleRoot = _merkleRoot;
         numOfWhiteListMembers = _numOfWhiteListMembers;
     }
@@ -89,14 +98,12 @@ contract AirDropToken is ERC721, ERC721Burnable, Ownable {
     function transferMultiple(address to, uint256[] memory tokenIds) external {
         for (uint256 i = 0; i < tokenIds.length; i++) {
             uint256 tokenId = tokenIds[i];
-            if(
-                ownerOf(tokenId) != msg.sender
-            ){
-                revert CustomError("Only the owner of this token can perform this action");
+            if (ownerOf(tokenId) != msg.sender) {
+                revert CustomError(
+                    "Only the owner of this token can perform this action"
+                );
             }
-            if(
-                to == address(0) && to == address(this)
-            ){
+            if (to == address(0) || to == address(this)) {
                 revert CustomError("Cannot send to zero or contract address");
             }
             transferFrom(msg.sender, to, tokenId);
@@ -109,15 +116,11 @@ contract AirDropToken is ERC721, ERC721Burnable, Ownable {
         uint index = uint256(uint160(msg.sender));
 
         // check if already claimed
-        if(
-            BitMaps.get(whitelistClaimed, index)
-        ){
+        if (BitMaps.get(whitelistClaimed, index)) {
             revert CustomError("Already Claimed!");
         }
         bytes32 leaf = keccak256(abi.encodePacked(msg.sender));
-        if(
-            !MerkleProof.verify(proof, merkleRoot, leaf)
-        ){
+        if (!MerkleProof.verify(proof, merkleRoot, leaf)) {
             revert CustomError("Invalid Merkle Proof");
         }
         BitMaps.setTo(whitelistClaimed, index, true);
@@ -131,31 +134,26 @@ contract AirDropToken is ERC721, ERC721Burnable, Ownable {
         timedTransitions
         atStage(Stages.PostMinting)
     {
-        uint index = memberIndex[msg.sender];
-        // check if already claimed
-        mintedTokens[index] = msg.sender;
+        uint index = uint256(uint160(msg.sender));
+        mintedTokens.push(msg.sender);
         revealBlockNumber[msg.sender] = block.number + 10;
         submitCommitment(keccak256(abi.encodePacked(index)));
     }
 
-    function submitCommitment(
-        bytes32 _commitment
-    ) private onlyBeforeReveal atStage(Stages.PreMinting) {
+    function submitCommitment(bytes32 _commitment) private onlyBeforeReveal {
         commitment[msg.sender] = _commitment;
         emit CommitmentSubmitted(_commitment);
     }
 
     function reveal() external onlyAfterReveal {
         uint tokenId = block.prevrandao;
-        if(
+        if (
             keccak256(abi.encodePacked(uint256(uint160(msg.sender)))) !=
-                commitment[msg.sender]
-        ){
+            commitment[msg.sender]
+        ) {
             revert CustomError("Incorrect Commitment");
         }
-        if(
-            BitMaps.get(revealedTokens, tokenId)
-        ){
+        if (BitMaps.get(revealedTokens, tokenId)) {
             revert CustomError("Token Already Revealed");
         }
 
@@ -166,21 +164,14 @@ contract AirDropToken is ERC721, ERC721Burnable, Ownable {
     function withdraw(
         uint256 _tokenId
     ) external onlyAfterReveal timedTransitions atStage(Stages.PostMinting) {
-        if(
-            BitMaps.get(didWithdraw, uint(uint160(msg.sender)))
-        ){
-            revert CustomError("Already Withdrawn");
+        if (ownerOf(_tokenId) != msg.sender) {
+            revert CustomError("Only the owner of this token can withdraw");
         }
-        BitMaps.setTo(didWithdraw, uint(uint160(msg.sender)), true);
         _burn(_tokenId);
         (bool success, ) = payable(msg.sender).call{value: 1 ether}("");
-        if(!success){
+        if (!success) {
             revert CustomError("Failed to send Ether");
         }
-    }
-
-    function getAllocation() public view returns (uint256) {
-        return nftAllocations[msg.sender];
     }
 
     function canReveal() public view returns (bool) {
@@ -188,11 +179,32 @@ contract AirDropToken is ERC721, ERC721Burnable, Ownable {
     }
 
     function nextStage() private {
-        if(
-            stage == Stages.NoSupply
-        ){
-            revert CustomError('Cannot transition from final state.');
+        if (stage == Stages.NoSupply) {
+            revert CustomError("Cannot transition from final state.");
         }
         stage = Stages(uint256(stage) + 1);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(
+        address to,
+        uint256 tokenId,
+        address auth
+    ) internal override(ERC721, ERC721Enumerable) returns (address) {
+        return super._update(to, tokenId, auth);
+    }
+
+    function _increaseBalance(
+        address account,
+        uint128 value
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._increaseBalance(account, value);
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC721, ERC721Enumerable) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
 }
--- assignment8/test/merkleTree.js
+++ assignment8/test/merkleTree.js
@@ -1,22 +1,20 @@
-// test/AirDropToken.test.js
-const { expect } = require('chai');
-const { ethers } = require('hardhat');
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 const { MerkleTree } = require('merkletreejs');
 const keccak256 = require('keccak256');
 
-
-describe('AirDropToken', function () {
+describe("AirDropToken", function () {
     let AirDropToken;
     let airDropToken;
-    let owner;
-    let user;
-    let rootHash;
-    let merkleTree;
+    let owner, user1;
+    let signers;
     let leafNodes;
+    let merkleTree;
+    let rootHash;
 
     beforeEach(async function () {
-        [owner, user] = await ethers.getSigners();
-        const signers = await ethers.getSigners();
+        [owner, user1] = await ethers.getSigners();
+        signers = await ethers.getSigners();
         let whitelistAddresses = signers.slice(0, 10);
         leafNodes = whitelistAddresses.map(addr => keccak256(addr.address));
         merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true });
@@ -24,40 +22,230 @@ describe('AirDropToken', function () {
         rootHash = merkleTree.getRoot();
 
         AirDropToken = await ethers.getContractFactory('AirDropToken');
-        airDropToken = await AirDropToken.deploy(rootHash, 10, whitelistAddresses.length);
+        airDropToken = await AirDropToken.deploy(rootHash, 20, whitelistAddresses.length, { value: ethers.parseEther("10") });
         await airDropToken.waitForDeployment();
     });
 
-    it('should deploy with the correct initial state', async function () {
-        expect(await airDropToken.totalSupply()).to.equal(10);
-        expect(await airDropToken.revealed()).to.equal(false);
-        expect(await airDropToken.numOfWhiteListMembers()).to.equal(10);
+    describe("Deployment", function () {
+        it("should deploy with the correct initial state", async function () {
+            expect(await airDropToken.totalSupplyLimit()).to.equal(20);
+            expect(await airDropToken.revealed()).to.equal(false);
+            expect(await airDropToken.numOfWhiteListMembers()).to.equal(10);
+            expect(await airDropToken.stage()).to.equal(0); // Stages.PreMinting
+        });
     });
 
-    it('should whitelist mint to a user', async function () {
-        const hexProof = merkleTree.getHexProof(leafNodes[1]);
-        await airDropToken.connect(user).whitelistMint(hexProof);
-        for (let i = 0; i < 10; i++) {
-            await ethers.provider.send("evm_mine", []);
-        }
-        await airDropToken.connect(user).reveal();
-        const balance = await airDropToken.balanceOf(user.address);
-        expect(balance).to.equal(1);
-        expect(await airDropToken.totalSupply()).to.equal(10);
+    describe("whitelistMint", function () {
+        describe("Validations", function () {
+            it("should allow whitelisted minting during PreMinting stage", async function () {
+                const hexProof = merkleTree.getHexProof(leafNodes[1]);
+                await airDropToken.connect(user1).whitelistMint(hexProof);
+                for (let i = 0; i < 10; i++) {
+                    await ethers.provider.send("evm_mine", []);
+                }
+                await airDropToken.connect(user1).reveal();
+                const balance = await airDropToken.balanceOf(user1.address);
+                expect(balance).to.equal(1);
+            });
+
+            it("Should revert with the right error if address already minted", async function () {
+
+                const hexProof = merkleTree.getHexProof(leafNodes[1]);
+                await airDropToken.connect(user1).whitelistMint(hexProof);
+                await expect(airDropToken.connect(user1).whitelistMint(hexProof)).to.be.revertedWithCustomError(airDropToken, "CustomError").withArgs("Already Claimed!");
+            });
+
+            it("Should not revert if sufficient balance", async function () {
+                const hexProof = merkleTree.getHexProof(leafNodes[1]);
+                await expect(airDropToken.connect(user1).whitelistMint(hexProof.slice(0, 2))).to.be.revertedWithCustomError(airDropToken, "CustomError").withArgs("Invalid Merkle Proof");
+            });
+
+            it("Should submit a commitment", async function () {
+                const hexProof = merkleTree.getHexProof(leafNodes[1]);
+                await airDropToken.connect(user1).whitelistMint(hexProof);
+                await expect(airDropToken.connect(user1).commitment(user1.address)).not.to.equal(0);
+            });
+        });
+        describe("Events", function () {
+            it("Should emit an event on submitting a commitment", async function () {
+                const hexProof = merkleTree.getHexProof(leafNodes[1]);
+                await expect(airDropToken.connect(user1).whitelistMint(hexProof)).to.emit(airDropToken, "CommitmentSubmitted");
+            });
+        });
+    });
+    describe("normalMint", function () {
+        describe("Validations", function () {
+            it("should not allow normal minting during PreMinting stage", async function () {
+                await expect(airDropToken.connect(user1).normalMint()).to.be.reverted;
+            });
+        });
+
+        describe("Events", function () {
+            it("Should emit an event on submitting a commitment", async function () {
+                for (let i = 0; i < 10; i++) {
+                    const hexProof = merkleTree.getHexProof(leafNodes[i]);
+                    await airDropToken.connect(signers[i]).whitelistMint(hexProof);
+                    for (let i = 0; i < 10; i++) {
+                        await ethers.provider.send("evm_mine", []);
+                    }
+                    await airDropToken.connect(signers[i]).reveal();
+                }
+                await expect(airDropToken.connect(user1).normalMint()).to.emit(airDropToken, "CommitmentSubmitted");
+            });
+        });
     });
 
-    it('should reveal after 10 blocks', async function () {
-        const hexProof = merkleTree.getHexProof(leafNodes[1]);
-        await airDropToken.connect(user).whitelistMint(hexProof);
-        for (let i = 0; i < 10; i++) {
-            await ethers.provider.send("evm_mine", []);
-        }
-        expect(await airDropToken.connect(user).reveal()); // Stages.PreMinting
+    describe("withdraw", function () {
+        describe("Validations", function () {
+            it("should not withdraw during PreMinting stage", async function () {
+                for (let i = 0; i < 5; i++) {
+                    const hexProof = merkleTree.getHexProof(leafNodes[i]);
+                    await airDropToken.connect(signers[i]).whitelistMint(hexProof);
+                    for (let i = 0; i < 10; i++) {
+                        await ethers.provider.send("evm_mine", []);
+                    }
+                    await airDropToken.connect(signers[i]).reveal();
+                }
+                const tokenId = await airDropToken.connect(user1).tokenOfOwnerByIndex(user1.address, 0)
+                await expect(airDropToken.connect(user1).withdraw(tokenId)).to.be.reverted;
+            });
+
+            it("should not withdraw if not owner", async function () {
+                for (let i = 0; i < 10; i++) {
+                    const hexProof = merkleTree.getHexProof(leafNodes[i]);
+                    await airDropToken.connect(signers[i]).whitelistMint(hexProof);
+                    for (let i = 0; i < 10; i++) {
+                        await ethers.provider.send("evm_mine", []);
+                    }
+                    await airDropToken.connect(signers[i]).reveal();
+                }
+                const tokenId = await airDropToken.connect(owner).tokenOfOwnerByIndex(owner.address, 0)
+                await expect(airDropToken.connect(user1).withdraw(tokenId)).to.be.revertedWithCustomError(airDropToken, "CustomError").withArgs("Only the owner of this token can withdraw");
+            });
+
+            it("should increase the sender balance by 1 ETH", async function () {
+                for (let i = 0; i < 10; i++) {
+                    const hexProof = merkleTree.getHexProof(leafNodes[i]);
+                    await airDropToken.connect(signers[i]).whitelistMint(hexProof);
+                    for (let i = 0; i < 10; i++) {
+                        await ethers.provider.send("evm_mine", []);
+                    }
+                    await airDropToken.connect(signers[i]).reveal();
+                }
+                const tokenId = await airDropToken.connect(user1).tokenOfOwnerByIndex(user1.address, 0)
+                const balanceBefore = await ethers.provider.getBalance(user1.address);
+                console.log(balanceBefore);
+                await airDropToken.connect(user1).withdraw(tokenId);
+                const balanceAfter = await ethers.provider.getBalance(user1.address);
+                console.log(balanceAfter);
+                expect(balanceAfter - balanceBefore).to.be.closeTo(ethers.parseEther("1"), ethers.parseEther("0.00008"));
+            });
+        });
+
+
+        describe("Events", function () {
+            it("Should emit an event on submitting a commitment", async function () {
+                for (let i = 0; i < 10; i++) {
+                    const hexProof = merkleTree.getHexProof(leafNodes[i]);
+                    await airDropToken.connect(signers[i]).whitelistMint(hexProof);
+                    for (let i = 0; i < 10; i++) {
+                        await ethers.provider.send("evm_mine", []);
+                    }
+                    await airDropToken.connect(signers[i]).reveal();
+                }
+                await expect(airDropToken.connect(user1).normalMint()).to.emit(airDropToken, "CommitmentSubmitted");
+            });
+        });
     });
 
-    it('should not mint to non-whitelisted addresses in the pre-minting stage', async function () {
-        const signers = await ethers.getSigners();
-        const test = signers[10];
-        await expect(airDropToken.connect(test).normalMint()).to.be.reverted;
+    describe("transferMultiple", function () {
+        describe("Validations", function () {
+            it("should not transfer if not owner", async function () {
+                for (let i = 0; i < 10; i++) {
+                    const hexProof = merkleTree.getHexProof(leafNodes[i]);
+                    await airDropToken.connect(signers[i]).whitelistMint(hexProof);
+                    for (let i = 0; i < 10; i++) {
+                        await ethers.provider.send("evm_mine", []);
+                    }
+                    await airDropToken.connect(signers[i]).reveal();
+                }
+                let tokenIds = [];
+                for (let i = 0; i < 3; i++) {
+                    await airDropToken.connect(owner).normalMint();
+                    for (let i = 0; i < 10; i++) {
+                        await ethers.provider.send("evm_mine", []);
+                    }
+                    await airDropToken.connect(owner).reveal();
+                    const tokenId = await airDropToken.connect(owner).tokenOfOwnerByIndex(owner.address, i);
+                    tokenIds.push(tokenId);
+                }
+                await expect(airDropToken.connect(user1).transferMultiple(owner, tokenIds)).to.be.revertedWithCustomError(airDropToken, "CustomError").withArgs("Only the owner of this token can perform this action");
+            });
+
+            it("should not transfer to zero or contract address", async function () {
+                for (let i = 0; i < 10; i++) {
+                    const hexProof = merkleTree.getHexProof(leafNodes[i]);
+                    await airDropToken.connect(signers[i]).whitelistMint(hexProof);
+                    for (let i = 0; i < 10; i++) {
+                        await ethers.provider.send("evm_mine", []);
+                    }
+                    await airDropToken.connect(signers[i]).reveal();
+                }
+                let tokenIds = [];
+                for (let i = 0; i < 3; i++) {
+                    await airDropToken.connect(owner).normalMint();
+                    for (let i = 0; i < 10; i++) {
+                        await ethers.provider.send("evm_mine", []);
+                    }
+                    await airDropToken.connect(owner).reveal();
+                    const tokenId = await airDropToken.connect(owner).tokenOfOwnerByIndex(owner.address, i);
+                    tokenIds.push(tokenId);
+                }
+                console.log(airDropToken.target)
+                await expect(airDropToken.connect(owner).transferMultiple(airDropToken.target, tokenIds)).to.be.revertedWithCustomError(airDropToken, "CustomError").withArgs("Cannot send to zero or contract address");
+            });
+        });
+
+        describe("Events", function () {
+            it("Should emit an event on submitting a commitment", async function () {
+                for (let i = 0; i < 10; i++) {
+                    const hexProof = merkleTree.getHexProof(leafNodes[i]);
+                    await airDropToken.connect(signers[i]).whitelistMint(hexProof);
+                    for (let i = 0; i < 10; i++) {
+                        await ethers.provider.send("evm_mine", []);
+                    }
+                    await airDropToken.connect(signers[i]).reveal();
+                }
+                await expect(airDropToken.connect(user1).normalMint()).to.emit(airDropToken, "CommitmentSubmitted");
+            });
+        });
+
+        describe("Transfers", function () {
+            it("Should transfer the funds", async function () {
+                for (let i = 0; i < 10; i++) {
+                    const hexProof = merkleTree.getHexProof(leafNodes[i]);
+                    await airDropToken.connect(signers[i]).whitelistMint(hexProof);
+                    for (let i = 0; i < 10; i++) {
+                        await ethers.provider.send("evm_mine", []);
+                    }
+                    await airDropToken.connect(signers[i]).reveal();
+                }
+                let tokenIds = [];
+                for (let i = 0; i < 3; i++) {
+                    await airDropToken.connect(owner).normalMint();
+                    for (let i = 0; i < 10; i++) {
+                        await ethers.provider.send("evm_mine", []);
+                    }
+                    await airDropToken.connect(owner).reveal();
+                    const tokenId = await airDropToken.connect(owner).tokenOfOwnerByIndex(owner.address, i);
+                    tokenIds.push(tokenId);
+                }
+                await airDropToken.connect(owner).transferMultiple(user1.address, tokenIds);
+                const ownerBalance = await airDropToken.balanceOf(owner.address);
+                const userBalance = await airDropToken.balanceOf(user1.address);
+                expect(userBalance).to.equal(ownerBalance + BigInt(tokenIds.length));
+            });
+        });
     });
 });
+
